a120adb5dce5be7cf4048400ea7619fdc6bed2a0
mbnsfr <mbnsfr@gmail.com>
2023-04-03 00:41:53 +0330

    add parse

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const ParseServer = require("parse-server").ParseServer;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +39,23 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const api = new ParseServer({
+  databaseURI: "mongodb://127.0.0.1:27017/uniserver", // Connection string for your MongoDB database
+  // cloud: "",
+  appId: "uniservermyAppId",
+  masterKey: "uniserverMasterKey", // Keep this key secret!
+  fileKey: "optionalFileKey",
+  serverURL: "http://localhost:1351/parse", // Don't forget to change to https if needed
+  // serverURL: "https://localhost:1351/parse", // Don't forget to change to https if needed
+  // javascriptKey: "",
+  // restAPIKey: "",
+});
+
+app.use("/parse", api);
+
+// app.listen(1351, function() {
+//   console.log(`uni-server running on port 1351.`);
+// });
 
 module.exports = app;
